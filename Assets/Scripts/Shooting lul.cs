0c9de2a8769a303069cad6707c2b6a61f1fe5bd7
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-03-14 13:50:34 +0100

    Update Shooting lul.cs

--- Assets/Scripts/Shooting lul.cs
+++ Assets/Scripts/Shooting lul.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shootinglul : MonoBehaviour
-{
+public class Shootinglul : MonoBehaviour {
     private GameObject player;
     private float damage = 1;
-    private void FireWeapon()
-    {
+    private void FireWeapon() {
         Vector2 origin = player.transform.position; //Make this an empty at the tip of the gun preferrably :D
         Vector2 direction = player.transform.forward;
         RaycastHit hitInfo;
@@ -15,9 +13,8 @@ public class Shootinglul : MonoBehaviour
         int layerMask = LayerMask.GetMask("Shootables"); //Enemies AND obstacles?
         Physics.Raycast(origin, direction, out hitInfo, maxDistance, layerMask);
 
-        if (hitInfo.collider?.gameObject is GameObject Enemy)
-        {
-            Enemy.Damage(damage);
+        if (hitInfo.collider?.gameObject is GameObject Enemy) {
+            // Enemy.Damage(damage);
         }
     }
 }
