654639ed9dae698564aea68d90d1def837e6760b
Stiftoad <64871902+Stiftoad@users.noreply.github.com>
2023-03-15 13:49:14 +0100

    minor sfx testing

--- Assets/EventUp.cs
+++ Assets/EventUp.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class EventUp : MonoBehaviour
 {
     private PlayerManager playerManager;
+    [SerializeField]
+    private AudioSource shoot;
     // Start is called before the first frame update
     void Start()
     {
         playerManager = GetComponentInParent<PlayerManager>();
+
     }
-    void Shoot() { playerManager.Shoot(); }
+    void Shoot() { playerManager.Shoot();shoot.pitch = Random.Range(0.95f,1.05f); shoot.Play(); }
 
 }
