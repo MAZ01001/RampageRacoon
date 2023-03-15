6dcc761a7f60e82a2af61da0923b3129f81e6aa8
Florian <99948959+O1aaf@users.noreply.github.com>
2023-03-15 01:45:10 +0100

    updated script "DrugEffectManager"

--- Assets/Scripts/DrugEffectManager.cs
+++ Assets/Scripts/DrugEffectManager.cs
@@ -40,5 +40,27 @@ public class DrugEffectManager : MonoBehaviour
             SceneManager.LoadScene("GameOverTest");
         }
     }
+
+    //wird aufgerufen vom Skript CollectableItemManager-Skript, timeToAdd wird dabei vom Skript mitgegeben
+    public void IncreaseTimer(float timeToAdd)
+    {
+        Debug.Log("IncreaseTimer called with timeToAdd: " + timeToAdd);
+        
+        // Erhöht den Timer um die gegebene Menge
+        Debug.Log("timer before: " + timer);
+        timer += timeToAdd;
+        Debug.Log("timer after: " + timer);
+
+        // Stellt sicher, dass der Timer nicht größer als die maximale Zeit ist
+        timer = Mathf.Min(timer, maxTime);
+
+        // Aktualisiert die Anzeige des Sliders
+        Debug.Log("Slider value before: " + timerSlider.value);
+        timerSlider.value = timer;
+        Debug.Log("Slider value after: " + timerSlider.value);
+        timerSlider.maxValue = maxTime;
+
+        //DIESES SKRIPT MUSS AN DEN PLAYER, NICHT AN DER DRUGEFFECTBAR SELBST!!!!!!
+    }
 }
 
