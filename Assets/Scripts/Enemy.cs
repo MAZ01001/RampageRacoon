244c10a1c82c6872c8c523b338130a986952b762
Stiftoad <64871902+Stiftoad@users.noreply.github.com>
2023-03-16 16:57:41 +0100

    Squirrel Enemy Movement and Animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,24 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    public float moveSpeed = 2f;        // Geschwindigkeit für Gegner
-    public int maxHealth = 100;         // Max Gesundheit Gegner
-    public int currentHealth;           // Aktuelle Gesundheit Gegner
-    public GameObject itemPrefab;       // Prefab vom DropItem (Gegnerdrop)
-    public float dropChance = 0.8f;     // Wahrscheinlichkeit für Itemdrop
+    [SerializeField]
+    private float moveSpeed = 2f;       // Geschwindigkeit für Gegner
+    [SerializeField]
+    private float moveSmooth;         // idfk
+    [SerializeField]
+    private int maxHealth = 100;        // Max Gesundheit Gegner
+    [SerializeField]
+    private int currentHealth;          // Aktuelle Gesundheit Gegner
+    [SerializeField]
+    private GameObject itemPrefab;      // Prefab vom DropItem (Gegnerdrop)
+    [SerializeField]
+    private float dropChance = 0.8f;    // Wahrscheinlichkeit für Itemdrop
+    [SerializeField]
+    private float detectRange;          // Reichweite in der Gegner den Spieler finden
 
+    private Vector2 smoothVelocity = Vector2.zero;       // idfk
     private Animator anim;              // Animator Gegners
+    private GameObject player;          // Player
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameManager.Instance.Player.gameObject;
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         currentHealth = maxHealth;
     }
 
     void Update()
     {
-        // Bewegung alter
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        bool inRange = (player.transform.position - this.transform.position).sqrMagnitude <= detectRange * detectRange;
+        if (inRange == true)
+        {
+            Vector2 moveDir = (player.transform.position - this.transform.position).normalized;
+            Vector2 moveDirSpeed = moveDir * this.moveSpeed;
+            this.rb.velocity = Vector2.SmoothDamp(
+                this.rb.velocity,
+                moveDirSpeed,
+                ref this.smoothVelocity,
+                this.moveSmooth
+                );
+        }
+        else if (inRange == false) { this.rb.velocity = Vector2.zero; }
+        anim.SetFloat("Speed", rb.velocity.magnitude);
+        if (rb.velocity.x > 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            sprite.flipX = false;
+        }
     }
 
     public void TakeDamage(int damage)
