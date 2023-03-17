0f1208bf5c8176dc727ca7c184cbd88160babf0a
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-03-17 15:55:52 +0100

    Update UIController.cs

--- Assets/Scripts/Sound/UIController.cs
+++ Assets/Scripts/Sound/UIController.cs
@@ -13,32 +13,32 @@ public class UIController : MonoBehaviour {
 
     //~ unity methods (private)
     private void Start() {
-        masterVolume.isOn = PlayerPrefs.GetInt("MasterToggle") == 1;
-        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
-        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
-        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        masterVolume.isOn = PlayerPrefs.GetInt("MasterToggle", 1) == 1;
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
     }
 
     //~ public methods
-    /// <summary> Toggles the master volume </summary>
+    /// <summary> Toggles the master volume and saves value in the <see cref="PlayerPrefs"/> </summary>
     public void ToggleMasterVolume(){
         SoundManager.Instance.ToggleMasterVolume(masterVolume.isOn);
         PlayerPrefs.SetInt("MasterToggle", masterVolume.isOn ? 1 : 0);
     }
 
-    /// <summary> Sets the master volume </summary>
+    /// <summary> Sets the master volume and saves value in the <see cref="PlayerPrefs"/> </summary>
     public void MasterVolume(){
         SoundManager.Instance.MasterVolume(musicSlider.value, sfxSlider.value, masterSlider.value);
         PlayerPrefs.SetFloat("MasterVolume", masterSlider.value);
     }
 
-    /// <summary> Sets the music volume </summary>
+    /// <summary> Sets the music volume and saves value in the <see cref="PlayerPrefs"/> </summary>
     public void MusicVolume(){
         SoundManager.Instance.MusicVolume(musicSlider.value, masterSlider.value);
         PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
     }
 
-    /// <summary> Sets the SFX volume </summary>
+    /// <summary> Sets the SFX volume and saves value in the <see cref="PlayerPrefs"/> </summary>
     public void SFXVolume(){
         SoundManager.Instance.SFXVolume(sfxSlider.value, masterSlider.value);
         PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
