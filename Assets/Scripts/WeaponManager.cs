f0e3da3b93b056a6d32f6642e27892c573148a40
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-03-17 04:28:38 +0100

    moved and renamed scripts

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+    //~ inspector (private)
+    [SerializeField][Tooltip("The shoot sfx")]
+    private AudioSource shoot;
+    
+    [SerializeField][Tooltip("If the gun is default facing to the right")]
+    private bool facingRight = true;
+
+    //~ private
+    private PlayerManager playerManager;
+    private SpriteRenderer sprite;
+
+    //~ unity methods (private)
+    private void Start() {
+        playerManager = GetComponentInParent<PlayerManager>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void FixedUpdate() {
+        // TODO gun look to mouse / arrow keys / bumpers
+        sprite.flipX = playerManager.IsFacingRight != this.facingRight;
+    }
+
+    //~ private methods
+    private void Shoot() {
+        playerManager.Shoot();
+        shoot.pitch = Random.Range(0.95f, 1.05f);
+        shoot.Play();
+    }
+}
