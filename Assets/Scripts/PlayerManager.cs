cb152f917cacff7474b0063d9f4f4935e187f549
MAZ <29178226+MAZ01001@users.noreply.github.com>
2023-03-18 19:38:10 +0100

    Update PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
@@ -42,6 +43,12 @@ public class PlayerManager : MonoBehaviour {
     [SerializeField][Tooltip("If the player is default facing to the right")]
     private bool facingRight = true;
 
+    [Header("Damage Anim")]
+    [SerializeField][Tooltip("Time in Seconds to fade between damage color and normal")]
+    private float damageEffectDecay;
+    [SerializeField][Tooltip("The particle to show when the player takes damage")]
+    private ParticleSystem particle;
+
     [Header("Movement")]
 
     //! removed from inspector
@@ -64,6 +71,9 @@ public class PlayerManager : MonoBehaviour {
     [SerializeField]
     [Tooltip("Damage Done per Shot")]
     private int damage;
+    [SerializeField]
+    [Tooltip("Max shoot distance")]
+    private float shootMaxDistance;
     //~ private
     private Animator anim;
     private InputManager inputManager;
@@ -72,6 +82,7 @@ public class PlayerManager : MonoBehaviour {
     private Matrix2x2 shearMatrix = Matrix2x2.Identity;
     private Vector2 smoothVelocity = Vector3.zero;
     private Enemy enemyScript;
+    private bool hurt = false;
 
     //~ public
     public bool IsFacingRight => this.spriteRenderer.flipX != this.facingRight;
@@ -87,6 +98,9 @@ public class PlayerManager : MonoBehaviour {
         //~ X axis
         Gizmos.color = Color.red;
         Gizmos.DrawRay(this.transform.position, (Vector3)this.shearMatrix.TransformVec2(Vector2.right));
+        //~ shoot range
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(this.transform.position, this.transform.right * this.shootMaxDistance);
     }
     private void Start() {
         //~ get components
@@ -126,13 +140,31 @@ public class PlayerManager : MonoBehaviour {
         Vector2 direction = this.transform.right;
         if (rb.velocity.x > 0.1f) direction = this.transform.right;
         else if (rb.velocity.x < -0.1f) direction = this.transform.right*-1;
-        float maxDistance = 500f;
         int layerMask = LayerMask.GetMask("Shootables"); //Enemies AND obstacles?
-        RaycastHit2D hitInfo = Physics2D.Raycast(origin, direction, maxDistance, layerMask);
+        RaycastHit2D hitInfo = Physics2D.Raycast(origin, direction, this.shootMaxDistance, layerMask);
         if (hitInfo.collider?.gameObject is GameObject enemy){
             Debug.Log("hit");
             enemyScript = enemy.GetComponent<Enemy>();
             enemyScript.Damage(damage);
+        }
+    }
+    /// <summary> [Coroutine] damages the player </summary>
+    /// <param name="damage"> the amount of damage </param>
+    public IEnumerator Damage(float damage){
+        if(!this.hurt){
+            this.hurt = true;
+            DrugEffectManager.Instance.IncreaseTimer(-damage);
+            SpriteRenderer spriteRenderer = this.GetComponent<SpriteRenderer>();
+            float timer = this.damageEffectDecay;
+            float dTimer = 0f;
+            particle.Play();
+            while(timer > 0f){
+                timer -= Time.deltaTime;
+                dTimer = Mathf.Lerp(1, 0, timer / this.damageEffectDecay);
+                spriteRenderer.color = new Color(1, dTimer, dTimer);
+                yield return null;
+            }
+            this.hurt = false;
         }
     }
 }
